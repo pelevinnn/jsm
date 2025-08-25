6a05889da33e63261c963ac3add7ff3bf41e49f7
pelevinnn <mosaviacentr@gmail.com>
2025-08-26 01:16:20 +0300

    fix(main): css

---
+++ design/index.php
@@ -0,0 +1,77 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Document</title>
+</head>
+<body>
+    <main>
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-6">
+                    <div class="wrapper d-flex justify-content-between">
+                        <div class="wrapper-main d-flex flex-column justify-content-between align-items-center w-50">
+                            <div class="wrapper-main__subtitle">
+                                <h4 class="light-lavender-color">Your result</h4>
+                            </div>
+                            <div class="wrapper-main__stat white-color">
+                                76
+                                <span class="light-lavender-color">of 100</span>
+                            </div>
+                            <div class="wrapper-main__title">
+                                <h2 class="white-color">Great</h2>
+                            </div>
+                            <div class="wrapper-main__info">
+                                <p class="light-lavender-color">You scored higher than 65% of the people who have taken these&nbsp;tests.</p>
+                            </div>
+
+                        </div>
+                        <div class="wrapper-stat d-flex flex-column justify-content-between align-items-center w-50 p-5">
+                            <div class="wrapper-stat__subtitle">
+                                <h4>Summary</h4>
+                            </div>
+                            <div class="wrapper-stat__list">
+                                <ul>
+                                    <li>
+                                        <div class="wrapper-stat__item">
+                                            <div class="wrapper-stat__name">Reaction</div>
+                                            <div class="wrapper-stat__numb">80 / 100</div>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div class="wrapper-stat__item">
+                                            <div class="wrapper-stat__name">Memory</div>
+                                            <div class="wrapper-stat__numb">92 / 100</div>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div class="wrapper-stat__item">
+                                            <div class="wrapper-stat__name">Verbal</div>
+                                            <div class="wrapper-stat__numb">61 / 100</div>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div class="wrapper-stat__item">
+                                            <div class="wrapper-stat__name">Visual</div>
+                                            <div class="wrapper-stat__numb">73 / 100</div>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class="wrapper-stat__btn">
+                                <button>Continue</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
+</body>
+</html>
